1fdbc1e80feeeae11c9442bfcb9af465c50783a8
Ricardo RCO <rcoproc@gmail.com>
2015-10-11 02:59:18 -0400

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the RKNET APP!"
       redirect_to @user
     else
