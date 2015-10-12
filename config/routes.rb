469833cce03f74b6754fc02619a70599c9561ce0
Ricardo RCO <rcoproc@gmail.com>
2015-10-12 00:56:10 -0400

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   resources :users
+  resources :microposts,  only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
